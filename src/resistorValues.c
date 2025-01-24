0aa1a9a6bce09edb88192d9b1d778860b5aecc28
ThatGarlicDude <garlicdudegaming@gmail.com>
2025-01-23 19:47:23 -0500

    Added the first bits of code.

---
+++ src/resistorValues.c
@@ -0,0 +1,6 @@
+typedef short ColorResistor;
+
+// Resistors used in the PCB.
+const ColorResistor RED_RESISTORS[] = {220, 470, 1000};
+const ColorResistor GREEN_RESISTORS[] = {220, 470, 1000};
+const ColorResistor BLUE_RESISTORS[] = {220, 470};
