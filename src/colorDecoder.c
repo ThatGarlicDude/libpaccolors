f93414ac169b4b8b41e314cbcaca549d81c195da
ThatGarlicDude <garlicdudegaming@gmail.com>
2025-01-30 21:34:53 -0500

    Added the colorDecoder files.

---
+++ src/colorDecoder.c
@@ -0,0 +1,6 @@
+#include <stdint.h>
+
+// Decodes a byte from the color ROM to an 8-bit RGB value.
+uint_fast8_t decodeByte(uint_fast8_t byte) {
+	return 0;
+}
---
+++ src/colorDecoder.h
@@ -0,0 +1,8 @@
+#ifndef COLOR_DECODER_H
+#define COLOR_DECODER_H
+
+	#include <stdint.h>
+
+	uint_fast8_t decodeByte(uint_fast8_t byte);
+
+#endif
